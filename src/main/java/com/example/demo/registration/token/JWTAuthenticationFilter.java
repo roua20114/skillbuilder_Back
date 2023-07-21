98d604642c7bb1dbe1ea446cae13b40fda20c99c
roua20114 <rouazarrad2017@gmail.com>
2023-07-21 11:43:52 +0200

    cors problem

---
+++ src/main/java/com/example/demo/registration/token/JWTAuthenticationFilter.java
@@ -0,0 +1,2 @@
+package com.example.demo.registration.token;public class JWTAuthenticationFilter {
+}
---
+++ src/main/java/com/example/demo/user/UserUserDetailsService.java
@@ -0,0 +1,7 @@
+package com.example.demo.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsService Implements UserDetailsService {
+}
